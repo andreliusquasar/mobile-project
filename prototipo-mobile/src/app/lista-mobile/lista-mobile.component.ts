76ffb35dbafc6772b3633190eab84491de229c12
andrelius <andrelius.quasar@gmail.com>
2018-09-08 16:05:57 -0300

    Carregando listas a partir do novo model de contrato

--- prototipo-mobile/src/app/lista-mobile/lista-mobile.component.ts
+++ prototipo-mobile/src/app/lista-mobile/lista-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Contrato } from './../models/contrato.model';
 
 @Component({
   selector: 'app-lista-mobile',
@@ -8,7 +9,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ListaMobileComponent implements OnInit {
 
   @Input()
-  recebeContratos = [];
+  recebeContratos: Array<Contrato>;
 
   constructor() { }
 
